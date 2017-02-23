348901f5ef0515f692216a2f954d0685813ba31f
fengyutongxing <070405sy>
2017-02-23 18:57:41 +0800

    zs 增加 OrderDetailsFragment

--- app/src/main/java/com/team/happysending/views/fragment/OrdersFragment.java
+++ app/src/main/java/com/team/happysending/views/fragment/OrdersFragment.java
@@ -1,13 +1,7 @@
 package com.team.happysending.views.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.team.happysending.R;
 
 /**
  * Created by zhang_shuai on 2017/2/23
@@ -17,13 +11,7 @@ import com.team.happysending.R;
 
 public class OrdersFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_orders,container,false);
 
-        return view;
 
-    }
 }
