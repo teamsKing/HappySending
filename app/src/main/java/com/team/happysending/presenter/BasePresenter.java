2790f69e84ac43ff8ce9f4b52a9752c217f4ad63
樊、先生 <skyfan1991@163.com>
2017-02-25 11:48:18 +0800

    樊 创建立即下单页面

--- app/src/main/java/com/team/happysending/presenter/BasePresenter.java
+++ app/src/main/java/com/team/happysending/presenter/BasePresenter.java
@@ -29,4 +29,6 @@ public abstract class BasePresenter<T extends BaseView> {
         this.someView = thisView;
     }
 
+
+
 }
--- app/src/main/java/com/team/happysending/utils/ToastUtils.java
+++ app/src/main/java/com/team/happysending/utils/ToastUtils.java
@@ -3,8 +3,6 @@ package com.team.happysending.utils;
 import android.content.Context;
 import android.widget.Toast;
 /**
- * @作者 : 杨玉安
- * @日期 : 2017/2/20 0020 11:25
  *  Toast工具类
  */
 public class ToastUtils {
