14b0d986a73c5e6594488fbd1bee86ad11d9232d
fengyutongxing <070405sy>
2017-02-27 21:41:12 +0800

    zs 修改 增加订单和历史界面的监听事件

--- app/src/main/java/com/team/happysending/views/activity/OrderDetailsActivity.java
+++ app/src/main/java/com/team/happysending/views/activity/OrderDetailsActivity.java
@@ -1,17 +1,52 @@
 package com.team.happysending.views.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.team.happysending.R;
 import com.team.happysending.presenter.OrderDetailsPresenter;
+import com.team.happysending.utils.ToastUtil;
 import com.team.happysending.views.interfaces.BaseView;
 import com.team.happysending.views.interfaces.OrderDetailsView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by zhang_shuai on 2017/2/24
- *
+ * <p>
  * 订单详情界面
  */
 
 public class OrderDetailsActivity extends BaseActivity<OrderDetailsPresenter> implements OrderDetailsView {
+    @BindView(R.id.detaild_shuqi)
+    TextView detaildShuqi;
+    @BindView(R.id.detaild_shushi)
+    TextView detaildShushi;
+    @BindView(R.id.details_wo_fa)
+    TextView detailsWoFa;
+    @BindView(R.id.detail_fa_shou)
+    TextView detailFaShou;
+    @BindView(R.id.detail_shuigou)
+    TextView detailShuigou;
+    @BindView(R.id.detail_shui)
+    TextView detailShui;
+    @BindView(R.id.detail_feiyong)
+    TextView detailFeiyong;
+    @BindView(R.id.detail_homebtn)
+    Button detailHomebtn;
+    @BindView(R.id.detail_home_jiedan)
+    ImageView detailHomeJiedan;
+    @BindView(R.id.detail_home_renwu)
+    Button detailHomeRenwu;
+    @BindView(R.id.detail_jiedan)
+    Button detail_jiedan;
+
     @Override
     protected int getLayout() {
         return R.layout.fragment_order_details;
@@ -35,5 +70,83 @@ public class OrderDetailsActivity extends BaseActivity<OrderDetailsPresenter> im
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.detaild_qi, R.id.detaild_shuqi, R.id.detaild_shushi, R.id.details_wo_fa, R.id.detail_fa_shou, R.id.detail_shuigou, R.id.detail_shui, R.id.detail_feiyong, R.id.detail_homebtn, R.id.detail_home_jiedan, R.id.detail_home_renwu,R.id.detail_jiedan})
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.detaild_qi:
+                break;
+            /**
+             * 发货地址
+             */
+            case R.id.detaild_shuqi:
+                break;
+            /**
+             * 收货地址
+             */
+            case R.id.detaild_shushi:
+                break;
+            /**
+             * 我的位置到发货的位置
+             */
+            case R.id.details_wo_fa:
+                break;
+            /**
+             * 发货的位置到收货位置的距离
+             */
+            case R.id.detail_fa_shou:
+                break;
+            /**
+             *要捎带的话  比如：水果
+             */
+            case R.id.detail_shuigou:
+                break;
+            /**
+             * 物品的类别
+             */
+            case R.id.detail_shui:
+                break;
+            /**
+             * 我要挣去的费用
+             */
+            case R.id.detail_feiyong:
+                break;
+            /**
+             * 回到主界面
+             */
+            case R.id.detail_homebtn:
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+            /**
+             * 接单的界面
+             */
+            case R.id.detail_home_jiedan:
+                startActivity(new Intent(this,OrderDetailsActivity.class));
+                finish();
+                break;
+            /**
+             * 历史任务的界面
+             */
+            case R.id.detail_home_renwu:
+                startActivity(new Intent(this,HistoryActivity.class));
+                finish();
+                break;
+            /**
+             * 本界面的接单
+             */
+            case R.id.detail_jiedan:
+                ToastUtil.show(this,"等待处理");
+                break;
+        }
     }
 }
--- app/src/main/java/com/team/happysending/views/activity/OrdersActivity.java
+++ app/src/main/java/com/team/happysending/views/activity/OrdersActivity.java
@@ -51,6 +51,14 @@ public class OrdersActivity extends BaseActivity<OrdersPresenter> implements Ord
     ImageView ordersHomeJiedan;
     @BindView(R.id.orders_home_renwu)
     Button ordersHomeRenwu;
+    /**
+     * 开始的地址
+     */
+    private TextView mQiShi;
+    /**
+     * 结束的地址
+     */
+    private TextView mJieShu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +78,11 @@ public class OrdersActivity extends BaseActivity<OrdersPresenter> implements Ord
         findViewById(R.id.orders_wo_fa).setOnClickListener(this);
         findViewById(R.id.orders_shuigou).setOnClickListener(this);
         findViewById(R.id.orders_fa_shou).setOnClickListener(this);
+        findViewById(R.id.orders_homebtn).setOnClickListener(this);
+        findViewById(R.id.orders_home_jiedan).setOnClickListener(this);
+        findViewById(R.id.orders_home_renwu).setOnClickListener(this);
+        mQiShi = (TextView) findViewById(R.id.orders_qishi_Address);
+        mJieShu = (TextView) findViewById(R.id.orders_jieshu_Address);
     }
 
     @Override
@@ -105,30 +118,71 @@ public class OrdersActivity extends BaseActivity<OrdersPresenter> implements Ord
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            //接单详情界面
+            /**
+             * 接单详情界面
+             */
             case R.id.orders_jiedan:
                 startActivity(new Intent(this,OrderDetailsActivity.class));
                 finish();
                 break;
+            /**
+             * 录取语音
+             */
             case R.id.orders_yuying:
                 ToastUtil.show(this,"语音");
                 break;
+            /**
+             * 物品类别
+             */
             case R.id.orders_leibian:
                 ToastUtil.show(this,"物品类别");
                 break;
+            /**
+             * 已加价格
+             */
             case R.id.orders_jiage:
                 ToastUtil.show(this,"价格");
                 break;
+            /**
+             * 我到取货的距离
+             */
             case R.id.orders_wo_fa:
                 ToastUtil.show(this,"我fa的距离");
                 break;
+            /**
+             * 水果有多少斤
+             */
             case R.id.orders_shuigou:
                 ToastUtil.show(this,"水果");
                 break;
+            /**
+             * 我从取货到收货人的距离
+             */
             case R.id.orders_fa_shou:
                 ToastUtil.show(this,"我shou的距离");
                 break;
 
+            /**
+             * 回到主页
+             */
+            case R.id.orders_homebtn:
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+            /**
+             * 留在本界面的接单
+             */
+            case R.id.orders_home_jiedan:
+                startActivity(new Intent(this,OrdersActivity.class));
+                finish();
+                break;
+            /**
+             * 跳转到历史任务界面
+             */
+            case R.id.orders_home_renwu:
+                startActivity(new Intent(this,HistoryActivity.class));
+                finish();
+                break;
         }
     }
 }
